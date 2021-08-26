f52a14a7df7dacbdf67bac84b863a1938a6afdad
vladetamirkovic <mirkovicvladeta2000@gmail.com>
2021-08-26 13:31:21 +0200

    card, player

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -70,14 +70,16 @@ Header.Search = function HeaderSearch({
     <Search {...restProps}>
       <SearchIcon
         onClick={() => setSearchActive((searchActive) => !searchActive)}
+        data-testid="search-click"
       >
         <img src="/images/icons/search.png" alt="Search" />
       </SearchIcon>
       <SearchInput
         value={searchTerm}
         onChange={({ target }) => setSearchTerm(target.value)}
-        placeholder="Search films and series.."
+        placeholder="Search films and series"
         active={searchActive}
+        data-testid="search-input"
       />
     </Search>
   );
